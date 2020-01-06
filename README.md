7a04dfcb0263e24f621b37bdc667126cb0034dc8
Willian Santos <m16@m16.com.br>
2020-01-06 08:04:58 -0300

    Star Project  (Commit)

---
+++ README.md
@@ -0,0 +1,22 @@
+# All development merit goes to https://github.com/gmurt & https://bitbucket.org/gmurt/kscomponents
+
+# KernowSoftwareFMX
+Kernow Software FMX Components for XE7, XE8, Delphi 10 Seattle, Berlin & Rio. 
+
+(May work with earlier versions but untested)
+
+Firemonkey Slide menu, ListView and Segment buttons for Delphi.  Inherits from a non-visual component to keep things simple.
+
+Includes demo with source.  Demo App also available on app store: 
+https://itunes.apple.com/gb/app/kscomponents-reference/id1031179701?mt=8
+
+### Feel free to support the components with a donation ;-)
+
+If you'd like to support the ksComponents project, you can do so at the following link ;-)
+
+http://www.kernow-software.co.uk/?page_id=397
+
+Any suggestions for updates are welcome :-) 
+
+Modified:
+Components of KernowSoftwareFMX for Delphi 10.3 Rio by m.me/WOS83
---
+++ Samples/Chat Bubbles/untMain.pas
@@ -0,0 +1,74 @@
+unit untMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls, FMX.Controls.Presentation, ksTableView,
+  FMX.Objects, FMX.Edit, FMX.Layouts, ksChatView, ksTypes;
+
+type
+  TForm24 = class(TForm)
+    ToolBar1: TToolBar;
+    Label1: TLabel;
+    Button2: TButton;
+    Image1: TImage;
+    Image2: TImage;
+    Button1: TButton;
+    ksChatView1: TksChatView;
+    procedure FormCreate(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Edit1KeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
+    procedure ksChatView1BeforePostText(Sender: TObject; var ABubble: TksChatBubbleInfo);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form24: TForm24;
+
+implementation
+
+uses System.UIConsts;
+{$R *.fmx}
+
+procedure TForm24.Button1Click(Sender: TObject);
+begin
+  ksChatView1.AddChatBubble('Hi There!', TksTableViewChatBubblePosition.ksCbpRight, $FFDDDDDD, claBlack, Image1.Bitmap);
+end;
+
+procedure TForm24.Button2Click(Sender: TObject);
+begin
+  ksChatView1.Clear;
+end;
+
+procedure TForm24.Edit1KeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
+begin
+  if Key = 13 then
+    Button1Click(Self);
+end;
+
+procedure TForm24.FormCreate(Sender: TObject);
+begin
+  Width := FormFactor.Width;
+  Height := FormFactor.Height;
+
+  Image1.Visible := False;
+  Image2.Visible := False;
+
+  ksChatView1.MyImage := Image2.Bitmap;
+
+  ksChatView1.AddChatBubble('Hi there, I thought I''d get in touch to let you know how great I think your components are, thanks a million!', ksCbpLeft, claDodgerblue, claWhite, Image2.Bitmap);
+  ksChatView1.AddChatBubble('Thanks! Currently working on a new form transition compoenent and also the ability to add user images to chat bubbles like you see here', ksCbpRight, $FFDDDDDD, claBlack, Image1.Bitmap);
+  ksChatView1.AddChatBubble('Wow!  '+#13+'You''re the man!!!', ksCbpLeft, claDodgerblue, claWhite, Image2.Bitmap);
+end;
+
+procedure TForm24.ksChatView1BeforePostText(Sender: TObject; var ABubble: TksChatBubbleInfo);
+begin
+  ABubble.Text := 'TEST_'+ABubble.Text;
+end;
+
+end.
